e9a633548101b4ab87f925c01d515b8b6ba5c4ec
Gabriel Vaquer <brielov@icloud.com>
2021-10-04 10:53:47 -0300

    Swap replaceAll with replace

--- src/cuit.ts
+++ src/cuit.ts
@@ -11,7 +11,7 @@ export type Type = typeof types[number];
  * @returns boolean
  */
 export const is = (input: string): boolean => {
-  const cuit = input.replaceAll(regx, estr);
+  const cuit = input.replace(regx, estr);
   if (cuit.length !== 11) return false;
   const d = 11 - (base.reduce((a, b, c) => a + ~~cuit[c] * b, 0) % 11);
   return ~~cuit[10] === (d === 11 ? 0 : d === 10 ? 0 : d);
@@ -24,7 +24,7 @@ export const is = (input: string): boolean => {
  * @returns string | null
  */
 export const guess = (input: string, type: Type): string | null => {
-  const dni = input.replaceAll(regx, estr).padStart(8, "0");
+  const dni = input.replace(regx, estr).padStart(8, "0");
   if (dni.length !== 8 || !types.includes(type)) return null;
   let xy = type === "F" ? "27" : type === "M" ? "20" : "30";
   const prefix = xy + dni;
@@ -52,7 +52,7 @@ export const guess = (input: string, type: Type): string | null => {
  */
 export const format = (dirty: string, separator = "-"): string => {
   if (!is(dirty)) throw new Error(`Invalid cuit: ${dirty}`);
-  const cuit = dirty.replaceAll(regx, "");
+  const cuit = dirty.replace(regx, "");
   const a = cuit.slice(0, 2);
   const b = cuit.slice(2, -1);
   const c = cuit.slice(-1);
